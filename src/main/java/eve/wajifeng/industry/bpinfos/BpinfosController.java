e5dcea4fb48ad418036e4cdfa0ea7fee78ba9779
zvtlt <zanvit.laurent@gmail.com>
2023-06-10 16:50:04 +0200

    README.MD

--- src/main/java/eve/wajifeng/industry/bpinfos/BpinfosController.java
+++ src/main/java/eve/wajifeng/industry/bpinfos/BpinfosController.java
@@ -20,7 +20,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.StringReader;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
